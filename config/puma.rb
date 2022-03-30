839f1c1d940b455fe3de3e2dc2d29b50c3049cdc
Torsten <torsten@rubydesign.fi>
2022-03-31 02:11:07 +0300

    more puma deploy

--- config/puma.rb
+++ config/puma.rb
@@ -22,9 +22,9 @@ port ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/puma/puma.pid" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/puma.pid" }
 
-bind 'unix://tmp/puma/puma.sock'
+bind 'unix://tmp/sockets/puma.sock'
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
