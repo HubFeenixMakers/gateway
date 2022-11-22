ee479cdffccc6357e79fc83d20c483b56372bd79
Torsten <torsten@rubydesign.fi>
2022-11-22 19:44:34 +0200

    first hero section on index

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -1,4 +1,10 @@
 class CmsController < ApplicationController
+
   def view
+    page = params[:id]
+    # assert file exists
+    @data = YAML.load_file(Rails.root.join('cms' , "#{page}.yaml"))
+    #assert data is an array (of hashes?)
   end
+
 end
--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -1,2 +1,12 @@
 module CmsHelper
+  # sectio should be hash with at least 'template' key
+  def find_template(section)
+    "sections/" + section["template"]
+  end
+
+  def bg(section)
+    #{'background-image' => url('#{image_url('cms/' + section['image'])}')}
+    img = image_url "cms/house.jpg"
+    "background-image: url('#{img}');"
+  end
 end
