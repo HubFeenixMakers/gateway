fbf07187b726b60474c74e5457e8fe720e7cbc37
Torsten <torsten@rubydesign.fi>
2022-11-23 11:05:22 +0200

    half section, split out button

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -9,4 +9,10 @@ module CmsHelper
     img = image_url "cms/house.jpg"
     "background-image: url('#{img}');"
   end
+  def image_for(section , classes)
+    image_tag("cms/#{section['image']}" , class: classes)
+  end
+  def has_button(section)
+    section['button']
+  end
 end
